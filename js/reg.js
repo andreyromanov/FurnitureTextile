246005b9a7e96312606bc20ebb7c03d98390233d
Andrii Romanov <andreygorogogo@gmail.com>
2019-05-23 09:41:02 +0300

    User profile. Registration table.

---
+++ js/reg.js
@@ -0,0 +1,57 @@
+
+var db;
+//database version setting
+var version = 1.0;
+//database name setting
+var dbName = "tizendb";
+//database display name setting
+var dbDisplayName = "tizen_test_db";
+//database size setting
+var dbSize = 2 * 1024 * 1024;
+
+
+function selectDB() {
+  if (window.openDatabase) {
+      //openDatabase(name, version, displayname, estimatedsize, callback);
+      db = openDatabase(dbName, version, dbDisplayName, dbSize);
+      dropTable(db);
+      createTable(db);
+
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      var pass = document.getElementById("password").value;
+
+      //inserting data in table
+      insertData(db, name, email, pass);
+      window.location.href = "profile.html";
+      
+  } else {
+      alert("Web SQL Database not supported in this browser");
+  }
+}
+
+
+// create table
+function createTable(db) {
+  db.transaction(function (t) {
+      t.executeSql("CREATE TABLE Reg_tbl (id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT)", []);
+  });
+}
+
+//inserting data in table
+function insertData(db, name, email, password) {
+  db.transaction(function (e) {
+      //var day = new Date();
+      e.executeSql("INSERT INTO Reg_tbl(name, email, password) VALUES (?, ?, ?)", [name, email, password], onSuccess, onError);
+  });
+}
+
+function onSuccess(e) { }
+function onError(e) { }
+
+// drop table
+function dropTable(db) {
+  db.transaction(function (e) {
+      e.executeSql("DROP TABLE Reg_tbl");
+  });
+}
